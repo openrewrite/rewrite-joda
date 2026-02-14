49548820f868d6718194b64f4ed1d06717f7053b
Tim te Beek <tim@moderne.io>
2026-02-14 21:10:32 +0100

    Update type table usage

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,12 +20,9 @@ dependencies {
     testImplementation("org.openrewrite:rewrite-maven")
 
     testRuntimeOnly("org.openrewrite:rewrite-java-21")
-    testRuntimeOnly("joda-time:joda-time:2.12.3")
-    testRuntimeOnly("org.threeten:threeten-extra:1.8.0")
 }
 
 recipeDependencies {
-    parserClasspath("joda-time:joda-time:2.12.3")
     parserClasspath("org.threeten:threeten-extra:1.8.0")
+    testParserClasspath("joda-time:joda-time:2.12.3")
 }
-
